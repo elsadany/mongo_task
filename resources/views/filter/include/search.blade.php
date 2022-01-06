8b72dc5d85c2c2a024ec74befdf7669db449409b
ahmedelsadany <eng.ahmedelsadany1@gmail.com>
2022-01-06 11:53:06 +0200

    commit

--- resources/views/filter/include/search.blade.php
+++ resources/views/filter/include/search.blade.php
@@ -1,101 +1,121 @@
-<form action="{{route('search')}}" method="get">
-    @csrf
-
-
-    <div id="items-area">
-        <div class="item">
-            <div class="row">
-                <div class="col-md-3 form-group">
-
-                    <select name="form[table][]" class="form-control " required="
-                            ">
-
-
-                        <option value="users">Users</option>
-
-                    </select>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Find a Partner <i class="fa fa-search" aria-hidden="true"></i></h3>
+                <a class="heading-elements-toggle"><i class="fa fa-ellipsis-v font-medium-3"></i></a>
+                <div class="heading-elements">
+                    <ul class="list-inline mb-0">
+                        <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                        <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                        <li><a data-action="close"><i class="ft-x"></i></a></li>
+                    </ul>
                 </div>
-                <div class="col-md-3">
-                    <div class="form-check">
+            </div>
+            <div class="card-content collapse show">
+                <div class="card-body">
+                    <form action="{{route('search')}}" method="get">
+                        @csrf
 
-                        <select name="form[column][]" class="form-control column" required="">
-                            @foreach(users_columns as $key=>$value)
-                            <option value="{{$key}}" @if(request()->has('form')&&request()->form['column'][0]==$key) selected @endif >{{$value}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
-                <div class="col-md-2 form-group">
-                    <select name="form[operator][]" class="form-control" required="">
-                        @foreach(operators as $operator)
-                        <option @if(request()->has('form')&&request()->form['operator'][0]==$operator) selected @endif>{{$operator}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="col-md-2 form-group">
 
-                    <input  type="text"name="form[value][]"  class="form-control value"  @if(request()->has('form')) value="{{request()->form['value'][0]}}" @endif required>
-                </div>
+                        <div id="items-area">
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-md-3 form-group">
 
+                                        <select name="form[table][]" class="form-control " required="
+                                                ">
+
+
+                                            <option value="users">Users</option>
+
+                                        </select>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-check">
+
+                                            <select name="form[column][]" class="form-control column" required="">
+                                                @foreach(users_columns as $key=>$value)
+                                                <option value="{{$key}}" @if(request()->has('form')&&request()->form['column'][0]==$key) selected @endif >{{$value}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2 form-group">
+                                        <select name="form[operator][]" class="form-control" required="">
+                                            @foreach(operators as $operator)
+                                            <option @if(request()->has('form')&&request()->form['operator'][0]==$operator) selected @endif>{{$operator}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="col-md-2 form-group">
+
+                                        <input  type="text"name="form[value][]"  class="form-control value"  @if(request()->has('form')) value="{{request()->form['value'][0]}}" @endif required>
+                                    </div>
+
+                                </div>
+                            </div>
+                            @if(request()->has('form')&&count(request()->form['column'])>1)
+                            @for($step=1;$step< count(request()->form['column']);$step++)
+                            <div class="item">
+
+                                <div class="row">
+
+                                    <div class="col-md-3 form-group">
+
+                                        <select name="form[table][]" class="form-control ">
+
+                                            <option value="users">Users</option>
+
+                                        </select>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-check">
+
+                                            <select name="form[column][]" class="form-control" required="">
+                                                @foreach(users_columns as $key=>$value)
+                                                <option value="{{$key}}" @if(request()->form['column'][$step]==$key) selected @endif>{{$value}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-2 form-group">
+                                        <select name="form[operator][]" class="form-control" required="">
+                                            @foreach(operators as $operator)
+                                            <option @if(request()->form['operator'][$step]==$operator) selected @endif>{{$operator}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="col-md-2 form-group">
+
+                                        <input type="text"name="form[value][]" value="{{request()->form['value'][$step]}}" class="form-control value" required>
+                                    </div>
+                                    <div class="col-md-2 form-group">
+                                        <select name="form[contact][]" class="form-control" required="">
+
+                                            <option @if(request()->form['contact'][$step-1]=='AND') selected @endif>AND</option>
+                                            <option @if(request()->form['contact'][$step-1]=='OR') selected @endif>OR</option>
+
+                                        </select>
+                                    </div> 
+                                    <a href="javascript:void()" class="item-remove"><i class="fa fa-times-circle text-danger"></i></a>
+                                </div>
+                            </div>
+                            @endfor
+                            @endif
+                        </div>
+                        <button type="button" id="add-more" class="btn btn-outline-primary btn-sm mt-1"><i class="fa fa-plus-circle" aria-hidden="true"></i> Add More</button>
+                        <div class="row">
+                            <div class=" mt-2 offset-md-10 col-md-2">
+                                <button type="submit" class="btn btn-primary" name="submit"><i class="fa fa-shopping-cart"></i> Search</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-        @if(request()->has('form')&&count(request()->form['column'])>1)
-        @for($step=1;$step< count(request()->form['column']);$step++)
-        <div class="item">
-
-            <div class="row">
-
-                <div class="col-md-3 form-group">
-
-                    <select name="form[table][]" class="form-control ">
-
-                        <option value="users">Users</option>
-
-                    </select>
-                </div>
-                <div class="col-md-3">
-                    <div class="form-check">
-
-                        <select name="form[column][]" class="form-control" required="">
-                            @foreach(users_columns as $key=>$value)
-                            <option value="{{$key}}" @if(request()->form['column'][$step]==$key) selected @endif>{{$value}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
-                <div class="col-md-2 form-group">
-                    <select name="form[operator][]" class="form-control" required="">
-                        @foreach(operators as $operator)
-                        <option @if(request()->form['operator'][$step]==$operator) selected @endif>{{$operator}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="col-md-2 form-group">
-
-                    <input type="text"name="form[value][]" value="{{request()->form['value'][$step]}}" class="form-control value" required>
-                </div>
-                <div class="col-md-2 form-group">
-                    <select name="form[contact][]" class="form-control" required="">
-
-                        <option @if(request()->form['contact'][$step-1]=='AND') selected @endif>AND</option>
-                        <option @if(request()->form['contact'][$step-1]=='OR') selected @endif>OR</option>
-
-                    </select>
-                </div> 
-                <a href="javascript:void()" class="item-remove"><i class="fa fa-times-circle text-danger"></i></a>
-            </div>
-        </div>
-        @endfor
-        @endif
     </div>
-    <button type="button" id="add-more" class="btn btn-outline-primary btn-sm mt-1"><i class="fa fa-plus-circle" aria-hidden="true"></i> Add More</button>
-    <div class="row">
-        <div class=" mt-2 offset-md-10 col-md-2">
-            <button type="submit" class="btn btn-primary" name="submit"><i class="fas fa-shopping-cart"></i> Search</button>
-        </div>
-    </div>
-</form>
-
+</div>
 <div id="blind" style="display:none">
     <div class="item">
         <div class="row">
